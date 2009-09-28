de5c2ed949bf4e059211bee6eb88bc48e93ab94b
Eric Steele <esteele@plone.org>
2009-09-28 19:29:25 +0000

    Remove the viewlet class. We don't need it.
    svn path=/plonetheme.sunburst/trunk/; revision=29966

--- plonetheme/sunburst/browser/viewlets.py
+++ plonetheme/sunburst/browser/viewlets.py
@@ -1,5 +1,2 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase, PersonalBarViewlet
-
-class NewPersonalBarViewlet(PersonalBarViewlet):
-    index = ViewPageTemplateFile('templates/personal_bar.pt')
+from plone.app.layout.viewlets.common import ViewletBase
