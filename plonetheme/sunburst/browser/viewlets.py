bfadf081d0508cddaa847070d071e37f14bbad4b
Eric Steele <esteele@plone.org>
2009-09-28 18:14:48 +0000

    Move custom personal_bar code into the theme product itself.
    svn path=/plonetheme.sunburst/trunk/; revision=29961

--- plonetheme/sunburst/browser/viewlets.py
+++ plonetheme/sunburst/browser/viewlets.py
@@ -1,3 +1,5 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
+from plone.app.layout.viewlets.common import ViewletBase, PersonalBarViewlet
 
+class NewPersonalBarViewlet(PersonalBarViewlet):
+    index = ViewPageTemplateFile('templates/personal_bar.pt')
