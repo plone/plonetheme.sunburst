d3f269eb40a8d78af3ae755040301504025eb4d0
Hanno Schlichting <hanno@hannosch.eu>
2010-07-07 13:59:11 +0000

    Removed remaining references to empty ``sunburst_js`` folder.
    svn path=/plonetheme.sunburst/trunk/; revision=37694

--- plonetheme/sunburst/tests/test_integration.py
+++ plonetheme/sunburst/tests/test_integration.py
@@ -1,23 +1,18 @@
-# Testing basic integration of the package into Plone
-
-from zope.component import getUtility
-
-from plone.portlets.interfaces import IPortletType
-
 from plonetheme.sunburst.tests.base import SunburstTestCase as TestCase
+
 
 class SunburstTestCase(TestCase):
 
     def test_sunburst_layers_available(self):
-        self.failUnless('sunburst_images' in self.portal.portal_skins.objectIds())
-        self.failUnless('sunburst_js' in self.portal.portal_skins.objectIds())
-        self.failUnless('sunburst_styles' in self.portal.portal_skins.objectIds())
-        self.failUnless('sunburst_templates' in self.portal.portal_skins.objectIds())                        
-        
+        self.failUnless('sunburst_images' in self.portal.portal_skins)
+        self.failUnless('sunburst_styles' in self.portal.portal_skins)
+        self.failUnless('sunburst_templates' in self.portal.portal_skins)
+
     def test_sunburst_skin_installed(self):
         self.skins = self.portal.portal_skins
         theme = self.skins.getDefaultSkin()
         self.failUnless(theme == 'Sunburst Theme', 'Default theme is %s' % theme)
+
 
 def test_suite():
     from unittest import defaultTestLoader
