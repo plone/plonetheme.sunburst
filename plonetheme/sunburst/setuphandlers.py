062686d04bb91f3731bfac1c6c6455c42a7ff889
Hanno Schlichting <hanno@hannosch.eu>
2011-01-18 15:17:56 +0000

    Adjust the new setuphandler introduced in 1.0.5 to avoid using copy/paste, which introduced unwanted additional security checks.
    svn path=/plonetheme.sunburst/trunk/; revision=46894

--- plonetheme/sunburst/setuphandlers.py
+++ plonetheme/sunburst/setuphandlers.py
@@ -1,16 +1,22 @@
+from Products.CMFCore.ActionInformation import Action
 from Products.CMFCore.utils import getToolByName
+
 
 def importVarious(context):
     # Make sure this import step only runs for the sunburst profile.
     if context.readDataFile('sunburst.txt') is None:
         return
-    
+
     # Copy the plone_setup action to the user category
     atool = getToolByName(context.getSite(), 'portal_actions')
-    cp = atool.site_actions.manage_copyObjects(['plone_setup'])
-    atool.user.manage_pasteObjects(cp)
-    
-    position = atool.user.getObjectPosition('preferences') + 1
-    atool.user.moveObjectToPosition('plone_setup', position)
+    user_category = atool.user
+    if 'plone_setup' in user_category:
+        return
 
-    atool.user.plone_setup.visible = True
+    state = dict(atool.site_actions.plone_setup.propertyItems())
+    state['visible'] = True
+    new_action = Action('plone_setup', **state)
+    user_category['plone_setup'] = new_action
+
+    position = user_category.getObjectPosition('preferences') + 1
+    user_category.moveObjectToPosition('plone_setup', position)
