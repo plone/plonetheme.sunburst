58fcba8c30926f69e8a446b58a1e72805e58c107
Eric Steele <esteele@plone.org>
2011-07-19 13:09:43 +0000

    Prepare plonetheme.sunburst 1.2.
    svn path=/plonetheme.sunburst/trunk/; revision=51342

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1.6dev'
+version = '1.2'
 
 long_description = (
     read('README.txt')
