97c423f5651938a1c6eb65763ffc115bc9a37825
Eric Steele <esteele@plone.org>
2011-08-25 12:23:52 +0000

    Back to development: 1.2.2
    svn path=/plonetheme.sunburst/trunk/; revision=51772

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.1'
+version = '1.2.2dev'
 
 long_description = (
     read('README.txt')
