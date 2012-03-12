ad30e49732757f4932b82fec501eb3c9bb36f0fc
Johannes Raggam <raggam-nl@adm.at>
2012-03-12 20:52:51 +0100

    backport of master changes until 9d9e75444ca7cc6ec62d4e7a9f9483e65d48e7d9,  Mon Feb 27 10:33:41 2012 +0100

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.2'
+version = '1.2.3dev'
 
 long_description = (
     read('README.txt')
