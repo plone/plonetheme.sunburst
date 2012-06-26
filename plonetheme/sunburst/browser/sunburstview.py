f136bd7342845e09d0ef46d30a3f98a3ccd49e59
Daniel Widerin <daniel@widerin.net>
2012-06-26 20:37:22 +0200

    fixed #12995

--- plonetheme/sunburst/browser/sunburstview.py
+++ plonetheme/sunburst/browser/sunburstview.py
@@ -15,31 +15,56 @@ class SunburstView(BrowserView):
     # Utility methods
 
     def getColumnsClass(self, view=None):
-        """Determine whether a column should be shown. The left column is called
-        plone.leftcolumn; the right column is called plone.rightcolumn.
+        """Determine whether a column should be shown. The left column is
+        called plone.leftcolumn; the right column is called plone.rightcolumn.
         """
-        context = aq_inner(self.context)
-        plone_view = getMultiAdapter((context, self.request), name=u'plone')
+
+        plone_view = getMultiAdapter(
+            (self.context, self.request), name=u'plone')
+        portal_state = getMultiAdapter(
+            (self.context, self.request), name=u'plone_portal_state')
+
         sl = plone_view.have_portlets('plone.leftcolumn', view=view);
         sr = plone_view.have_portlets('plone.rightcolumn', view=view);
-        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+
+        isRTL = portal_state.is_rtl()
+
+        # pre-fill dictionary
+        columns = dict(one="", content="", two="")
 
         if not sl and not sr:
             # we don't have columns, thus conten takes the whole width
-            return "cell width-full position-0"
+            columns['content'] = "width-full position-0"
+
         elif sl and sr:
             # In case we have both columns, content takes 50% of the whole
             # width and the rest 50% is spread between the columns
-            return "cell width-1:2 position-1:4"
-        elif (sr and not sl) and (portal_state.is_rtl()):
+            columns['one'] = "width-1:4 position-0"
+            columns['content'] = "width-1:2 position-1:4"
+            columns['two'] = "width-1:4 position-0"
+
+        elif (sr and not sl) and isRTL:
             # We have right column and we are in RTL language
-            return "cell width-3:4 position-1:4"
-        elif (sr and not sl) and (not portal_state.is_rtl()):
+            columns['content'] = "width-3:4 position-1:4"
+            columns['two'] = "width-1:4 position-0"
+
+        elif (sr and not sl) and not isRTL:
             # We have right column and we are NOT in RTL language
-            return "cell width-3:4 position-0"
-        elif (sl and not sr) and (portal_state.is_rtl()):
+            columns['content'] = "width-3:4 position-0"
+            columns['two'] = "width-1:4 position-3:4"
+
+        elif (sl and not sr) and isRTL:
             # We have left column and we are in RTL language
-            return "cell width-3:4 position-0"
-        elif (sl and not sr) and (not portal_state.is_rtl()):
+            columns['one'] = "width-1:4 position-3:4"
+            columns['content'] = "width-3:4 position-0"
+
+        elif (sl and not sr) and not isRTL:
             # We have left column and we are in NOT RTL language
-            return "cell width-3:4 position-1:4"
+            columns['one'] = "width-1:4 position-0"
+            columns['content'] = "width-3:4 position-1:4"
+
+        # append cell to each css-string
+        for key, value in columns.items():
+            columns[key] = "cell " + value
+
+        return columns
