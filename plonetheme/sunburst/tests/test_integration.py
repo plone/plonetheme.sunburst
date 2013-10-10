298e6cf608588a5ea3b995dbc027d1dcdde47250
tisto <tisto@plone.org>
2013-10-10 16:53:36 +0200

    Replace deprecated test assert statements.

--- plonetheme/sunburst/tests/test_integration.py
+++ plonetheme/sunburst/tests/test_integration.py
@@ -4,14 +4,14 @@ from plonetheme.sunburst.tests.base import SunburstTestCase as TestCase
 class SunburstTestCase(TestCase):
 
     def test_sunburst_layers_available(self):
-        self.failUnless('sunburst_images' in self.portal.portal_skins)
-        self.failUnless('sunburst_styles' in self.portal.portal_skins)
-        self.failUnless('sunburst_templates' in self.portal.portal_skins)
+        self.assertTrue('sunburst_images' in self.portal.portal_skins)
+        self.assertTrue('sunburst_styles' in self.portal.portal_skins)
+        self.assertTrue('sunburst_templates' in self.portal.portal_skins)
 
     def test_sunburst_skin_installed(self):
         self.skins = self.portal.portal_skins
         theme = self.skins.getDefaultSkin()
-        self.failUnless(theme == 'Sunburst Theme', 'Default theme is %s' % theme)
+        self.assertTrue(theme == 'Sunburst Theme', 'Default theme is %s' % theme)
 
 
 def test_suite():
